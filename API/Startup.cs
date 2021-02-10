8b97d166e8e329eb320322953b53485c531bb4c2
jl <jose-luis.delrio.altran@axa.com>
2021-02-10 10:56:09 +0100

    08

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,4 +1,5 @@
 using API.Extensions;
+using API.Interfaces;
 using API.Middleware;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -22,7 +23,6 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddApplicationServices(_configuration);
 
             services.AddControllers();
